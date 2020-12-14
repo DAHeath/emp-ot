f8b5aebf660b313f6e0410664166c258355672ce
David Heath <heath.davidanthony@gmail.com>
2020-12-14 09:43:22 -0500

    inline single point

--- emp-ot/ferret/lpn_error.h
+++ emp-ot/ferret/lpn_error.h
@@ -112,21 +112,55 @@ void lpn_error(
 
   // execute the single-point OTs in parallel
   std::vector<std::thread> ths;
-  for(int i = 0; i < threads+1; ++i) {
-    int start = i * width;
-    int end = std::min((std::size_t)(i+1)*width, desc.t);
+  for (std::size_t i = 0; i < threads+1; ++i) {
+    std::size_t start = i * width;
+    std::size_t end = std::min((std::size_t)(i+1)*width, desc.t);
     ths.emplace_back(std::thread { [&, start, end] {
-      for (int j = start; j < end; ++j) {
+      GT::PRG prg;
+      for (std::size_t j = start; j < end; ++j) {
+        std::bitset<128>* ggm_tree = sparse_vector+j*leave_n;
         std::span<std::bitset<128>> pad = blocks;
         pad = pad.subspan(j * 2*desc.bin_sz, 2*desc.bin_sz);
 
         if constexpr (role == Role::Sender) {
-          auto [secret_sum_f2, m] = error_point_send<model>(
-              tree_height,
-              sparse_vector+j*leave_n,
-              delta,
-              consist_check_VW.data()+j);
-          secret_sums_f2[j] = secret_sum_f2;
+          std::bitset<128> seed = prg();
+          std::vector<std::bitset<128>> m((tree_height-1) * 2);
+
+          // generate GGM tree from the top
+          {
+            auto ot_msg_0 = m.data();
+            auto ot_msg_1 = m.data() + tree_height - 1;
+            ggm_expand(seed, ggm_tree[0], ggm_tree[1]);
+            ot_msg_0[0] = ggm_tree[0];
+            ot_msg_1[0] = ggm_tree[1];
+            for (std::size_t h = 1; h < tree_height-1; ++h) {
+              ot_msg_0[h] = ot_msg_1[h] = 0;
+              std::size_t sz = 1<<h;
+              for (int i = sz-1; i >= 0; --i) {
+                ggm_expand(ggm_tree[i], ggm_tree[i*2], ggm_tree[i*2+1]);
+                ot_msg_0[h] ^= ggm_tree[i*2];
+                ot_msg_1[h] ^= ggm_tree[i*2+1];
+              }
+            }
+          }
+
+          secret_sums_f2[j] = delta;
+          std::bitset<128> one = (std::bitset<128> { 1 }).flip();
+          for (std::size_t i = 0; i < leave_n; ++i) {
+            ggm_tree[i] &= one;
+            secret_sums_f2[j] ^= ggm_tree[i];
+          }
+
+          if (model == Model::Malicious) {
+            // consistency check
+            std::vector<std::bitset<128>> chi(leave_n);
+            Hash hash;
+            block digest[2];
+            hash.hash_once(digest, &secret_sums_f2[j], sizeof(block));
+            uni_hash_coeff_gen((block*)chi.data(), digest[0], leave_n);
+
+            vector_inn_prdt_sum_red((block*)&consist_check_VW[j], (block*)chi.data(), (block*)ggm_tree, leave_n);
+          }
 
           auto m0 = m.data();
           auto m1 = &m[tree_height-1];
@@ -146,15 +180,53 @@ void lpn_error(
             m[i] = pre_data[k+i] ^ pad[2*i + b[i]];
           }
 
-          auto [chi_alpha, W] = error_point_recv<model>(
-              m,
-              secret_sums_f2[j],
-              tree_height,
-              positions[j]%leave_n,
-              bs.get() + j*(tree_height-1),
-              sparse_vector+j*leave_n);
-          consist_check_chi_alpha[j] = chi_alpha;
-          consist_check_VW[j] = W;
+          { // gmm tree reconstruction
+            int to_fill_idx = 0;
+            for (int i = 0; i < tree_height-1; ++i) {
+              // reconstruct a layer of the ggm tree
+              to_fill_idx = to_fill_idx * 2;
+              ggm_tree[to_fill_idx] = ggm_tree[to_fill_idx+1] = 0;
+              int item_n = 1<< (i+1);
+
+              std::bitset<128> nodes_sum = 0;
+              for (std::size_t j = b[i] != 0; j < item_n; j+=2) {
+                nodes_sum ^= ggm_tree[j];
+              }
+
+              ggm_tree[to_fill_idx + b[i]] = nodes_sum ^ m[i];
+              if (i+1 != tree_height-1) {
+                for (int j = item_n-1; j >= 0; --j) {
+                  ggm_expand(ggm_tree[j], ggm_tree[j*2], ggm_tree[j*2+1]);
+                }
+              }
+              to_fill_idx += !b[i];
+            }
+          }
+
+          int choice_pos = positions[j]%leave_n;
+          ggm_tree[choice_pos] = 0;
+          std::bitset<128> one = (std::bitset<128> { 1 }).flip();
+          std::bitset<128> nodes_sum = 0;
+          for(int i = 0; i < leave_n; ++i) {
+            ggm_tree[i] &= one;
+            nodes_sum ^= ggm_tree[i];
+          }
+          ggm_tree[choice_pos] = nodes_sum ^ secret_sums_f2[j];
+
+          if (model == Model::Malicious) {
+            // check consistency
+            std::vector<std::bitset<128>> chi(leave_n);
+            Hash hash;
+            block digest[2];
+            hash.hash_once(digest, &secret_sums_f2[j], sizeof(block));
+            uni_hash_coeff_gen((block*)chi.data(), digest[0], leave_n);
+            auto chi_alpha = chi[choice_pos];
+            std::bitset<128> W;
+            vector_inn_prdt_sum_red((block*)&W, (block*)chi.data(), (block*)ggm_tree, leave_n);
+
+            consist_check_chi_alpha[j] = chi_alpha;
+            consist_check_VW[j] = W;
+          }
         }
       }}});
   }
