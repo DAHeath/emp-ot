6e2996a1b186ca2c3c57b3595ed9fe0bb4e00836
David Heath <heath.davidanthony@gmail.com>
2021-01-02 14:11:22 -0500

    factored out to link

--- src/ferret.h
+++ src/ferret.h
@@ -37,7 +37,7 @@ static constexpr MpDesc PRE = {
 
 template <Model model, Role role>
 std::vector<std::bitset<128>> base_cot(
-    NetIO& io,
+    Link& link,
     std::bitset<128> delta,
     bool* choices,
     std::size_t n) {
@@ -46,15 +46,13 @@ std::vector<std::bitset<128>> base_cot(
   if constexpr (role == Role::Sender) {
     block delta2;
     memcpy(&delta2, &delta, sizeof(delta));
-    NetLink link { &io };
     IKNP::send<model>(link, delta, buffer);
-    io.flush();
+    link.flush();
     for(int i = 0; i < n; ++i) {
       buffer[i] = buffer[i] & minusone;
     }
 
   } else {
-    NetLink link { &io };
     IKNP::recv<model>(link, buffer.data(), choices, n);
     std::bitset<128> ch[2];
     ch[0] = 0;
@@ -77,7 +75,7 @@ struct FerretCOT {
   std::bitset<128> delta;
   std::vector<std::bitset<128>> small_correlation;
 
-  static FerretCOT make(NetIO& io, GT::PRG& prg) {
+  static FerretCOT make(Link& link, GT::PRG& prg) {
     FerretCOT out;
 
     if constexpr (role == Role::Sender) {
@@ -90,31 +88,31 @@ struct FerretCOT {
       prg.random_bool(choices.get(), PRE.m);
     }
 
-    auto init = base_cot<model, role>(io, out.delta, choices.get(), PRE.m);
+    auto init = base_cot<model, role>(link, out.delta, choices.get(), PRE.m);
 
     out.small_correlation.resize(PRE.n);
-    out.lpn_extension(PRE, io, prg, out.small_correlation, init);
+    out.lpn_extension(PRE, link, prg, out.small_correlation, init);
 
     return out;
   }
 
-  std::size_t extend(NetIO& io, GT::PRG& prg, std::span<std::bitset<128>> buf) {
+  std::size_t extend(Link& link, GT::PRG& prg, std::span<std::bitset<128>> buf) {
     if (buf.size() < REGULAR.n || (buf.size() - REGULAR.m) % REGULAR.limit != 0) {
       error("Insufficient space. Use `byte_memory_need_inplace` to compute needed space.");
     }
     std::size_t ot_output_n = buf.size() - REGULAR.m;
     std::size_t round = ot_output_n / REGULAR.limit;
     for (std::size_t i = 0; i < round; ++i) {
-      lpn_extension(REGULAR, io, prg, buf, small_correlation);
+      lpn_extension(REGULAR, link, prg, buf, small_correlation);
       buf = buf.subspan(REGULAR.limit);
       std::copy(buf.begin(), buf.begin() + REGULAR.m, small_correlation.begin());
     }
     return ot_output_n;
   }
 
-  std::vector<std::bitset<128>> extend(NetIO& io, GT::PRG& prg, std::size_t n) {
+  std::vector<std::bitset<128>> extend(Link& link, GT::PRG& prg, std::size_t n) {
     std::vector<std::bitset<128>> out(byte_memory_need_inplace(n));
-    extend(io, prg, out);
+    extend(link, prg, out);
     return out;
   }
 
@@ -125,7 +123,7 @@ struct FerretCOT {
 
   void lpn_extension(
       const MpDesc& desc,
-      NetIO& io,
+      Link& link,
       GT::PRG& prg,
       std::span<std::bitset<128>> tar,
       std::span<std::bitset<128>> src) {
@@ -133,13 +131,12 @@ struct FerretCOT {
     { // gen seed
       if constexpr (role == Role::Sender) {
         seed = prg();
-        io.send_data(&seed, sizeof(std::bitset<128>));
+        link.send((const std::byte*)&seed, sizeof(std::bitset<128>));
       } else {
-        io.recv_data(&seed, sizeof(std::bitset<128>));
+        link.recv((std::byte*)&seed, sizeof(std::bitset<128>));
       }
-      io.flush();
+      link.flush();
     }
-    NetLink link { &io };
     lpn_error<model, role, threads>(desc, link, prg, delta, tar.data(), src);
     sparse_linear_code<role>(desc, seed, threads, tar, src.subspan(CONSIST_CHECK_COT_NUM));
   }
--- test/ot.cc
+++ test/ot.cc
@@ -4,25 +4,23 @@
 
 
 template <typename T>
-double test_rcot(T* ot, NetIO& io, int party, int length) {
+double test_rcot(T* ot, Link& link, int party, int length) {
   GT::PRG prg;
 
   auto start = clock_start();
-
-  auto b = ot->extend(io, prg, length);
+  auto b = ot->extend(link, prg, length);
 
   long long t = time_from(start);
-  io.sync();
   if (party == ALICE) {
-    io.send_block((block*)&ot->delta, 1);
-    io.send_block((block*)b.data(), b.size());
+    link.send((const std::byte*)&ot->delta, 16);
+    link.send((const std::byte*)b.data(), 16*b.size());
   }
   else if (party == BOB) {
     std::bitset<128> ch[2];
     ch[0] = 0;
     std::bitset<128>* b0 = new std::bitset<128>[b.size()];
-    io.recv_block((block*)ch+1, 1);
-    io.recv_block((block*)b0, b.size());
+    link.recv((std::byte*)(ch+1), 16);
+    link.recv((std::byte*)b0, 16*b.size());
     for (size_t i = 0; i < b.size(); ++i) {
       b[i] ^= ch[b[i][0]];
     }
@@ -45,28 +43,30 @@ int main(int argc, char** argv) {
 
   if (party == ALICE) {
     NetIO io { nullptr, port };
+    NetLink link { &io };
     {
       GT::PRG prg;
-      auto ferretcot = FerretCOT<Model::Semihonest, Role::Sender, threads>::make(io, prg);
-      std::cout <<"Passive FERRET OT\t"<<double(length)/test_rcot(&ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+      auto ferretcot = FerretCOT<Model::Semihonest, Role::Sender, threads>::make(link, prg);
+      std::cout <<"Passive FERRET OT\t"<<double(length)/test_rcot(&ferretcot, link, party, length)*1e6<<" OTps"<<endl;
     }
 
     {
       GT::PRG prg;
-      auto ferretcot = FerretCOT<Model::Malicious, Role::Sender, threads>::make(io, prg);
-      std::cout <<"Active FERRET OT\t"<<double(length)/test_rcot(&ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+      auto ferretcot = FerretCOT<Model::Malicious, Role::Sender, threads>::make(link, prg);
+      std::cout <<"Active FERRET OT\t"<<double(length)/test_rcot(&ferretcot, link, party, length)*1e6<<" OTps"<<endl;
     }
   } else {
     NetIO io { "127.0.0.1",port };
+    NetLink link { &io };
     {
       GT::PRG prg;
-      auto ferretcot = FerretCOT<Model::Semihonest, Role::Receiver, threads>::make(io, prg);
-      std::cout <<"Passive FERRET OT\t"<<double(length)/test_rcot(&ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+      auto ferretcot = FerretCOT<Model::Semihonest, Role::Receiver, threads>::make(link, prg);
+      std::cout <<"Passive FERRET OT\t"<<double(length)/test_rcot(&ferretcot, link, party, length)*1e6<<" OTps"<<endl;
     }
     {
       GT::PRG prg;
-      auto ferretcot = FerretCOT<Model::Malicious, Role::Receiver, threads>::make(io, prg);
-      std::cout <<"Active FERRET OT\t"<<double(length)/test_rcot(&ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+      auto ferretcot = FerretCOT<Model::Malicious, Role::Receiver, threads>::make(link, prg);
+      std::cout <<"Active FERRET OT\t"<<double(length)/test_rcot(&ferretcot, link, party, length)*1e6<<" OTps"<<endl;
     }
   }
 }
